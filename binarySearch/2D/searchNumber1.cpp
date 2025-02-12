6fe1386d9c268882d9fd0f5e40e37ba426913f4e
Kartik-Bhutra <kartikbhutra02@gmail.com>
2025-02-12 13:22:13 +0530

    correcting the order

--- binarySearch/2D/searchNumber1.cpp
+++ binarySearch/2D/searchNumber1.cpp
@@ -3,23 +3,40 @@
 using namespace std;
 bool search(vector<vector<int>> &mat, int target)
 {
-  int n = mat.size(), m = mat[0].size() - 1;
-  int i = 0, j = m;
-  while (i < n && j >= 0)
+  int low = 0, high = mat.size() - 1;
+  int rowIdx = -1;
+  while (high >= low)
   {
-    if (mat[i][j] == target)
-      return true;
-    else if (mat[i][j] > target)
-      j--;
+    int mid = (high + low) / 2;
+    if (mat[mid][0] <= target)
+    {
+      low = mid + 1;
+      rowIdx = mid;
+    }
     else
-      i++;
+      high = mid - 1;
+  }
+  if (rowIdx != -1)
+  {
+    low = 0, high = mat[0].size() - 1;
+    while (high >= low)
+    {
+      int mid = (high + low) / 2;
+      if (mat[rowIdx][mid] == target)
+        return true;
+      else if (mat[rowIdx][mid] < target)
+        low = mid + 1;
+      else
+        high = mid - 1;
+    }
+    return false;
   }
   return false;
 }
 int main()
 {
   vector<vector<int>> mat = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
-  // time complexity is O(N+M) and space complexity is O(1)
+  // time complexity is O(Log(N) + Log(M)) and space complexity is O(1)
   cout << search(mat, 14) << '\n';
   return 0;
 }
--- binarySearch/2D/searchNumber2.cpp
+++ binarySearch/2D/searchNumber2.cpp
@@ -1,12 +1,25 @@
-// https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+// https://leetcode.com/problems/search-a-2d-matrix/description/
 #include <bits/stdc++.h>
 using namespace std;
 bool search(vector<vector<int>> &mat, int target)
 {
-  
+  int n = mat.size(), m = mat[0].size() - 1;
+  int i = 0, j = m;
+  while (i < n && j >= 0)
+  {
+    if (mat[i][j] == target)
+      return true;
+    else if (mat[i][j] > target)
+      j--;
+    else
+      i++;
+  }
+  return false;
 }
 int main()
 {
-  vector<vector<int>> mat = {{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+  vector<vector<int>> mat = {{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}};
+  // time complexity is O(N+M) and space complexity is O(1)
+  cout << search(mat, 14) << '\n';
   return 0;
 }
